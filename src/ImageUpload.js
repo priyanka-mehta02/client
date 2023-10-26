e81e476fca792079765ed3e890c300c44a51f277
priyanka-mehta02 <122629880+priyanka-mehta02@users.noreply.github.com>
2023-10-26 21:47:50 +0200

    image display

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -1,45 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function ImageUpload() {
-    const [selectedFile, setSelectedFile] = useState(null);
-    const [message, setMessage] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [message, setMessage] = useState("");
 
-    const handleFileChange = (event) => {
-        setSelectedFile(event.target.files[0]);
-    };
-
-    const handleUpload = () => {
-        if (!selectedFile) {
-            setMessage('Please select a file to upload.');
-            return;
+  
+    const handleFileChange= (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          const imageUrl = URL.createObjectURL(file);
+          setSelectedFile(imageUrl);
         }
+  };
 
-        const formData = new FormData();
-        formData.append('avatar', selectedFile);
+  const handleUpload = () => {
+    if (!selectedFile) {
+      setMessage("Please select a file to upload.");
+      return;
+    }
 
-        fetch(`${process.env.REACT_APP_BE_URL}/upload`, {
-            method: 'POST',
-            body: formData,
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log(data);
-                setMessage(data.message);
-            })
-            .catch((error) => {
-                console.log(error);
-                setMessage('An error occurred during the upload.');
-            });
-    };
+    const formData = new FormData();
+    formData.append("avatar", selectedFile);
 
-    return (
-        <div>
-            <h1>Image Upload</h1>
-            <input type="file"  name ="avatar"accept="image/*" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Upload Image</button>
-            <p>{message}</p>
-        </div>
-    );
+    fetch(`${process.env.REACT_APP_BE_URL}/upload`, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.log(error);
+        setMessage("An error occurred during the upload.");
+      });
+  };
+
+  return (
+    <div>
+      <h1>Image Upload</h1>
+      <input
+        type="file"
+        name="avatar"
+        accept="image/*"
+        onChange={handleFileChange}
+      />
+       <div >
+        <img src={selectedFile} alt="Selected"  style={{ width: '300px', height: '200px' }} />
+      </div>
+      <button onClick={handleUpload}>Upload Image</button>
+      <p>{message}</p>
+    </div>
+  );
 }
 
 export default ImageUpload;
